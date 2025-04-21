5392610ddf53c9f234a7f6c1ae0d9192ee4435d5
GustavoJST <gutojusto22@gmail.com>
2025-04-20 23:05:01 -0300

    Add annotations pkg

---
+++ pkg/annotations/annotations.go
@@ -0,0 +1,7 @@
+package annotations
+
+const (
+	ProtectedIngressAnnotation  = "kube-botblocker.github.io/protectedIngress"
+	IngressConfigNameAnnotation = "kube-botblocker.github.io/ingressConfigName"
+	IngressServerSnippet        = "nginx.ingress.kubernetes.io/server-snippet"
+)
