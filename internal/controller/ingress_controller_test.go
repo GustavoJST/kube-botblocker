88393830fcab99b492452d676da6dfaec1762ce8
GustavoJST <gutojusto22@gmail.com>
2025-06-24 21:29:04 -0300

    Move general test functions and variables to test suite

--- internal/controller/ingress_controller_test.go
+++ internal/controller/ingress_controller_test.go
@@ -18,40 +18,16 @@ package controller
 
 import (
 	"fmt"
-	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	"github.com/GustavoJST/kube-botblocker/api/v1alpha1"
-	"github.com/GustavoJST/kube-botblocker/pkg/annotations"
 )
 
 var _ = Describe("Ingress Controller", Ordered, func() {
-	const (
-		timeout  = 10 * time.Second
-		duration = 10 * time.Second
-		interval = 250 * time.Millisecond
-
-		defaultTestNamespace = "default"
-		operatorNamespace    = "kube-botblocker"
-	)
-
 	var (
-		ingConfNameAnn   = annotations.IngressConfigNameAnnotation
-		ingSpecHashAnn   = annotations.IngressConfigSpecHash
-		serverSnippetAnn = annotations.IngressServerSnippet
-
-		blockedAgents = []string{
-			"GoogleBot", "AI2Bot", "Ai2Bot-Dolma",
-			"Amazonbot", "omgili", "omgilibot",
-		}
-
 		baseExpectedSnippet = `# kube-botblocker.github.io operator: Configuration start
 # Configuration added by kube-botblocker operator. Do not edit any of this manually
 if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilibot)") {
@@ -66,148 +42,16 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 
 	namespace := corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: operatorNamespace,
+			Name: defaultOperatorNamespace,
 		},
 	}
 
-	type testContext struct {
-		ingressConfig v1alpha1.IngressConfig
-		ingress       networkingv1.Ingress
-	}
-
-	makeTestName := func(base string, suffix int) string {
-		return fmt.Sprintf("%s-%d-%d", base, GinkgoRandomSeed(), suffix)
-	}
-
-	createIngressConfig := func(baseName string) v1alpha1.IngressConfig {
-		name := makeTestName(baseName, GinkgoParallelProcess())
-		key := types.NamespacedName{
-			Name:      name,
-			Namespace: operatorNamespace,
-		}
-
-		ingressConfig := v1alpha1.IngressConfig{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "IngressConfig",
-				APIVersion: "kube-botblocker.github.io/v1alpha1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      key.Name,
-				Namespace: key.Namespace,
-			},
-			Spec: v1alpha1.IngressConfigSpec{
-				BlockedUserAgents: blockedAgents,
-			},
-		}
-
-		Expect(k8sClient.Create(ctx, &ingressConfig)).To(Succeed())
-		DeferCleanup(func() {
-			Expect(k8sClient.Delete(ctx, &ingressConfig)).To(Succeed())
-		})
-
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, key, &ingressConfig)).To(Succeed())
-		}, timeout, interval).Should(Succeed())
-
-		return ingressConfig
-	}
-
-	createIngress := func(baseName string, namespace string, annotations map[string]string) networkingv1.Ingress {
-		if namespace == "" {
-			namespace = defaultTestNamespace
-		}
-		name := makeTestName(baseName, GinkgoParallelProcess())
-		ingress := networkingv1.Ingress{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:        name,
-				Namespace:   namespace,
-				Annotations: annotations,
-			},
-			Spec: networkingv1.IngressSpec{
-				Rules: []networkingv1.IngressRule{{}},
-			},
-		}
-
-		Expect(k8sClient.Create(ctx, &ingress)).To(Succeed())
-		DeferCleanup(func() {
-			Expect(k8sClient.Delete(ctx, &ingress)).To(Succeed())
-		})
-
-		return ingress
-	}
-
-	setupDefaultTestContext := func(baseName string, ingressAnnotations map[string]string) testContext {
-		ingressConfig := createIngressConfig(baseName)
-
-		if ingressAnnotations == nil {
-			ingressAnnotations = make(map[string]string)
-		}
-		if _, exists := ingressAnnotations[ingConfNameAnn]; !exists {
-			ingressAnnotations[ingConfNameAnn] = ingressConfig.Name
-		}
-
-		ingress := createIngress(baseName, "", ingressAnnotations)
-
-		return testContext{
-			ingressConfig: ingressConfig,
-			ingress:       ingress,
-		}
-	}
-
-	verifyServerSnippet := func(ingress *networkingv1.Ingress, expected string) {
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
-			g.Expect(ingress.GetAnnotations()[serverSnippetAnn]).To(Equal(expected))
-		}, timeout, interval).Should(Succeed())
-	}
-
-	verifyServerSnippetAbsent := func(ingress *networkingv1.Ingress) {
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
-			g.Expect(metav1.HasAnnotation(ingress.ObjectMeta, serverSnippetAnn)).To(
-				BeFalseBecause("server-snippet annotation should not be present"),
-			)
-		}, timeout, interval).Should(Succeed())
-	}
-
-	verifySpecHash := func(ingress *networkingv1.Ingress, ingressConfig *v1alpha1.IngressConfig) {
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingressConfig), ingressConfig)).To(Succeed())
-			g.Expect(ingressConfig.Status.SpecHash).To(Not(BeEmpty()))
-		}, timeout, interval).Should(Succeed())
-
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
-			g.Expect(ingress.GetAnnotations()[ingSpecHashAnn]).NotTo(BeEmpty())
-		}, timeout, interval).Should(Succeed())
-
-		Eventually(func(g Gomega) {
-			g.Expect(ingressConfig.Status.SpecHash).To(Equal(ingress.GetAnnotations()[ingSpecHashAnn]))
-		}, timeout, interval).Should(Succeed())
-	}
-
-	verifySpecHashAbsent := func(ingress *networkingv1.Ingress) {
-		Eventually(func(g Gomega) {
-			g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
-			g.Expect(metav1.HasAnnotation(ingress.ObjectMeta, ingSpecHashAnn)).To(
-				BeFalseBecause("IngressConfigSpecHash annotation should not be present"),
-			)
-		}, timeout, interval).Should(Succeed())
-	}
-
-	updateIngressAnnotations := func(ingress *networkingv1.Ingress, updateFunc func(map[string]string)) {
-		ann := ingress.GetAnnotations()
-		if ann == nil {
-			ann = make(map[string]string)
-		}
-		updateFunc(ann)
-		ingress.SetAnnotations(ann)
-		Expect(k8sClient.Update(ctx, ingress)).To(Succeed())
-	}
-
 	BeforeAll(func() {
+		k8sClient.Delete(ctx, &namespace)
 		By("Creating operator namespace")
-		Expect(k8sClient.Create(ctx, &namespace)).To(Succeed())
+		Eventually(func(g Gomega) {
+			g.Expect(k8sClient.Create(ctx, &namespace)).To(Succeed())
+		}, timeout, interval).Should(Succeed())
 	})
 
 	Describe(fmt.Sprintf("When %s='false'", currentNsOnlyEnv), Label(fmt.Sprintf("%s=false", currentNsOnlyEnv)), func() {
@@ -219,7 +63,7 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&tc.ingressConfig), &tc.ingressConfig)).To(Succeed())
 
 				By("Verifying Ingress SpecHash is the same as IngressConfig SpechHash")
-				verifySpecHash(&tc.ingress, &tc.ingressConfig)
+				verifySpecHashMatch(&tc.ingress, &tc.ingressConfig)
 			})
 
 			It("Should add NGINX configuration to server-snippet", func() {
@@ -270,7 +114,7 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(&tc.ingressConfig), &tc.ingressConfig)).To(Succeed())
 
 				By("Verifying Ingress SpecHash has been restored and is the same as IngressConfig SpechHash")
-				verifySpecHash(&tc.ingress, &tc.ingressConfig)
+				verifySpecHashMatch(&tc.ingress, &tc.ingressConfig)
 
 			})
 		})
@@ -279,7 +123,7 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 			It("Should add NGINX configuration to server-snippet", func() {
 				By("Setting up test context without annotations")
 				tc := testContext{
-					ingressConfig: createIngressConfig("ing-existing-default"),
+					ingressConfig: createIngressConfig("ing-existing-default", nil),
 					ingress:       createIngress("ing-existing-default", "", nil),
 				}
 
@@ -298,7 +142,7 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 					serverSnippetAnn: existingSnippet,
 				}
 				tc := testContext{
-					ingressConfig: createIngressConfig("ing-existing-append"),
+					ingressConfig: createIngressConfig("ing-existing-append", nil),
 					ingress:       createIngress("ing-existing-append", "", annotations),
 				}
 
@@ -357,7 +201,7 @@ if ($http_user_agent ~* "(GoogleBot|AI2Bot|Ai2Bot-Dolma|Amazonbot|omgili|omgilib
 			It("Should only process IngressConfigs in the same namespace", func() {
 				By("Creating Ingress in the same namespace as the operator")
 				tc := testContext{
-					ingressConfig: createIngressConfig("ing-current-ns-only-same-ns"),
+					ingressConfig: createIngressConfig("ing-current-ns-only-same-ns", nil),
 					ingress:       createIngress("ing-current-ns-only-same-ns", "", nil),
 				}
 
--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -18,13 +18,18 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -34,6 +39,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/GustavoJST/kube-botblocker/api/v1alpha1"
+	"github.com/GustavoJST/kube-botblocker/pkg/annotations"
 	"github.com/GustavoJST/kube-botblocker/pkg/environment"
 	// +kubebuilder:scaffold:imports
 )
@@ -50,6 +56,24 @@ var (
 
 	currentNsOnlyEnv = "CURRENT_NAMESPACE_ONLY"
 	OperatorNsEnv    = "OPERATOR_NAMESPACE"
+
+	defaultTestNamespace     = "default"
+	defaultOperatorNamespace = "kube-botblocker"
+
+	ingConfNameAnn   = annotations.IngressConfigNameAnnotation
+	ingSpecHashAnn   = annotations.IngressConfigSpecHash
+	serverSnippetAnn = annotations.IngressServerSnippet
+
+	defaultBlockedAgents = []string{
+		"GoogleBot", "AI2Bot", "Ai2Bot-Dolma",
+		"Amazonbot", "omgili", "omgilibot",
+	}
+)
+
+const (
+	timeout  = 10 * time.Second
+	duration = 10 * time.Second
+	interval = 250 * time.Millisecond
 )
 
 func TestControllers(t *testing.T) {
@@ -145,4 +169,144 @@ func getFirstFoundEnvTestBinaryDir() string {
 		}
 	}
 	return ""
+}
+
+type testContext struct {
+	ingressConfig v1alpha1.IngressConfig
+	ingress       networkingv1.Ingress
+}
+
+func makeTestName(base string, suffix int) string {
+	return fmt.Sprintf("%s-%d-%d", base, GinkgoRandomSeed(), suffix)
+}
+
+// If blockedAgents is nil, use default user agents list for IngressConfig creation
+func createIngressConfig(baseName string, blockedAgents []string) v1alpha1.IngressConfig {
+	if blockedAgents == nil {
+		blockedAgents = defaultBlockedAgents
+	}
+
+	name := makeTestName(baseName, GinkgoParallelProcess())
+	key := types.NamespacedName{
+		Name:      name,
+		Namespace: defaultOperatorNamespace,
+	}
+
+	ingressConfig := v1alpha1.IngressConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "IngressConfig",
+			APIVersion: "kube-botblocker.github.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.Name,
+			Namespace: key.Namespace,
+		},
+		Spec: v1alpha1.IngressConfigSpec{
+			BlockedUserAgents: blockedAgents,
+		},
+	}
+
+	Expect(k8sClient.Create(ctx, &ingressConfig)).To(Succeed())
+	DeferCleanup(func() {
+		Expect(k8sClient.Delete(ctx, &ingressConfig)).To(Succeed())
+	})
+
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, key, &ingressConfig)).To(Succeed())
+	}, timeout, interval).Should(Succeed())
+
+	return ingressConfig
+}
+
+func createIngress(baseName string, namespace string, annotations map[string]string) networkingv1.Ingress {
+	if namespace == "" {
+		namespace = defaultTestNamespace
+	}
+	name := makeTestName(baseName, GinkgoParallelProcess())
+	ingress := networkingv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   namespace,
+			Annotations: annotations,
+		},
+		Spec: networkingv1.IngressSpec{
+			Rules: []networkingv1.IngressRule{{}},
+		},
+	}
+
+	Expect(k8sClient.Create(ctx, &ingress)).To(Succeed())
+	DeferCleanup(func() {
+		Expect(k8sClient.Delete(ctx, &ingress)).To(Succeed())
+	})
+
+	return ingress
+}
+
+func setupDefaultTestContext(baseName string, ingressAnnotations map[string]string) testContext {
+	ingressConfig := createIngressConfig(baseName, nil)
+
+	if ingressAnnotations == nil {
+		ingressAnnotations = make(map[string]string)
+	}
+	if _, exists := ingressAnnotations[ingConfNameAnn]; !exists {
+		ingressAnnotations[ingConfNameAnn] = ingressConfig.Name
+	}
+
+	ingress := createIngress(baseName, "", ingressAnnotations)
+
+	return testContext{
+		ingressConfig: ingressConfig,
+		ingress:       ingress,
+	}
+}
+
+func verifyServerSnippet(ingress *networkingv1.Ingress, expected string) {
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
+		g.Expect(ingress.GetAnnotations()[serverSnippetAnn]).To(Equal(expected))
+	}, timeout, interval).Should(Succeed())
+}
+
+func verifyServerSnippetAbsent(ingress *networkingv1.Ingress) {
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
+		g.Expect(metav1.HasAnnotation(ingress.ObjectMeta, serverSnippetAnn)).To(
+			BeFalseBecause("server-snippet annotation should not be present"),
+		)
+	}, timeout, interval).Should(Succeed())
+}
+
+func verifySpecHashMatch(ingress *networkingv1.Ingress, ingressConfig *v1alpha1.IngressConfig) {
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingressConfig), ingressConfig)).To(Succeed())
+		g.Expect(ingressConfig.Status.SpecHash).To(Not(BeEmpty()))
+	}, timeout, interval).Should(Succeed())
+
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
+		g.Expect(ingress.GetAnnotations()[ingSpecHashAnn]).NotTo(BeEmpty())
+	}, timeout, interval).Should(Succeed())
+
+	Eventually(func(g Gomega) {
+		g.Expect(ingressConfig.Status.SpecHash).To(Equal(ingress.GetAnnotations()[ingSpecHashAnn]))
+	}, timeout, interval).Should(Succeed())
+}
+
+func verifySpecHashAbsent(ingress *networkingv1.Ingress) {
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(ingress), ingress)).To(Succeed())
+		g.Expect(metav1.HasAnnotation(ingress.ObjectMeta, ingSpecHashAnn)).To(
+			BeFalseBecause("IngressConfigSpecHash annotation should not be present"),
+		)
+	}, timeout, interval).Should(Succeed())
+}
+
+func updateIngressAnnotations(ingress *networkingv1.Ingress, updateFunc func(map[string]string)) {
+	ann := ingress.GetAnnotations()
+	if ann == nil {
+		ann = make(map[string]string)
+	}
+	updateFunc(ann)
+	ingress.SetAnnotations(ann)
+	Expect(k8sClient.Update(ctx, ingress)).To(Succeed())
 }
